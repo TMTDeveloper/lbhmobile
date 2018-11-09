42ebf8b9f7daa510a8407df3396f7af59e8b23f1
Reza Aulia <zenhuwmank@gmail.com>
2018-11-09 11:38:40 +0700

    firstcommit

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,86 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { File } from "@ionic-native/file";
+import {
+  DocumentViewer,
+  DocumentViewerOptions
+} from "@ionic-native/document-viewer";
+import { FileTransfer } from "@ionic-native/file-transfer";
+import { FileOpener } from "@ionic-native/file-opener";
+import { BackendService } from "../../providers/backend.service";
+import { LoadingController } from "ionic-angular";
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+  selector: "page-about",
+  templateUrl: "about.html"
 })
 export class AboutPage {
+  documents = [];
+  constructor(
+    private document: DocumentViewer,
+    private file: File,
+    private transfer: FileTransfer,
+    public navCtrl: NavController,
+    private opener: FileOpener,
+    private service: BackendService,
+    public loadingCtrl: LoadingController
+  ) {}
 
-  constructor(public navCtrl: NavController) {
-
+  ngAfterViewInit() {
+    this.getDocument();
   }
 
+  presentLoading() {
+    const loader = this.loadingCtrl.create({
+      content: "Please wait...",
+      duration: 3000
+    });
+    loader.present();
+  }
+
+  getDocument() {
+    const loader = this.loadingCtrl.create({
+      content: "Please wait...",
+      duration: 3000
+    });
+    loader.present();
+    this.service.getreq("files/pdf/files").subscribe(response => {
+      if (response != null) {
+        this.documents = response;
+        loader.dismiss();
+      }
+    });
+  }
+
+  getDocumentRefresh(refresher) {
+    // const loader = this.loadingCtrl.create({
+    //   content: "Please wait..."
+    // });
+    // loader.present();
+    this.service.getreq("files/pdf/files").subscribe(response => {
+      if (response != null) {
+        this.documents = response;
+        refresher.complete();
+        // loader.dismiss();
+      }
+    });
+  }
+
+  downloadAndOpenPdf(file) {
+    console.log(file);
+    let path = this.file.dataDirectory;
+
+    const transfer = this.transfer.create();
+    transfer
+      .download(
+        "http://188.166.199.153/webadmin/api/files/pdf/download/" + file,
+        path + file
+      )
+      .then(entry => {
+        let url = entry.toURL();
+        this.opener
+          .open(url, "application/pdf")
+          .then(() => console.log("success"));
+        // this.document.viewDocument(url, "application/pdf", {});
+      });
+  }
 }
---
+++ src/providers/backend.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from "@angular/core";
+import { Http, Headers, RequestOptions } from "@angular/http";
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Rx';
+@Injectable()
+export class BackendService {
+  // baseurlxpay:string='http://202.158.20.141:5001/xpay-service/api/'
+
+  baseurl: string = "http://188.166.199.153/webadmin/api/";
+  moni: boolean = false;
+  constructor(public http: Http) {}
+
+  getreq(url: string) {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/x-www-form-urlencoded");
+
+    const options = new RequestOptions({
+      headers: headers
+    });
+    return this.http.get(this.baseurl + url, options).map(res => res.json());
+  }
+
+  postreq(url: string, body) {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+
+    const options = new RequestOptions({
+      headers: headers
+    });
+    return this.http
+      .post(this.baseurl + url, body, options)
+      .map(res => res.json());
+  }
+
+  patchreq(url: string, body) {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+
+    const options = new RequestOptions({
+      headers: headers
+    });
+    return this.http
+      .patch(this.baseurl + url, body, options)
+      .map(res => res.json());
+  }
+}
